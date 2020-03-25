b886d4879b99e78ced5870041794016225cf6c8f
kphu1301 <ken.phu.217@my.csun.edu>
2020-03-24 19:14:26 -0700

    cors

--- src/main/java/com/kphu1301/hangman/game/GameController.java
+++ src/main/java/com/kphu1301/hangman/game/GameController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @Controller
 public class GameController {
 	
