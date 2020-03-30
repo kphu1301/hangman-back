cbebb978c7fcaf503231410a1f31143110ee0509
klp1301 <ken.phu.217@my.csun.edu>
2020-03-29 22:04:36 -0700

    ...

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,204 +1,25 @@
-class UI {
-  constructor() {
-    this.playGameEl = document.querySelector(".play-btn");
-    this.titleEl = document.querySelector("#title");
-    this.containerEl = document.querySelector(".container");
-  }
-  showWordHidden(word, gameDiv) {
-    const categoryDiv = document.createElement("div");
-    categoryDiv.className = "category";
-    categoryDiv.innerHTML = `
-    <h2>Category: ${gameState.category}
-    `;
-    gameDiv.appendChild(categoryDiv);
+//ui elements
+const playBtn = document.querySelector("#play-btn");
+const category = document.querySelector("#category");
+const bodyParts = document.querySelectorAll(".body-part");
+const lettersPlayed = document.querySelector("#letters-played");
+const wordContainer = document.querySelector(".word-container");
 
-    const guessesLeftDiv = document.createElement("div");
-    guessesLeftDiv.className = "guesses-left";
-    guessesLeftDiv.innerHTML = `
-      <h3>Guesses Left: ${gameState.currNumGuesses}</h4>
-    `;
-    gameDiv.appendChild(guessesLeftDiv);
+const words = ["application", "pale", "background", "packers"];
+let correctLetters = ["a"];
+let word = words[Math.floor(Math.random() * words.length)];
 
-    const wordDiv = document.createElement("div");
-    wordDiv.className = "word-container";
-    for (let i = 0; i < word.length; i++) {
-      let letterDiv = document.createElement("div");
-      if (word[i] === " ") {
-        letterDiv.className = "word-letter space";
-      } else {
-        letterDiv.className = "word-letter";
-      }
+showWord();
 
-      wordDiv.appendChild(letterDiv);
-    }
-    gameDiv.appendChild(wordDiv);
-  }
-
-  initGame(word) {
-    //create game container
-    const gameDiv = document.createElement("div");
-    gameDiv.className = "game-container";
-
-    //show word
-    ui.showWordHidden(word, gameDiv);
-
-    //show user input
-    ui.showInputLetters(gameDiv);
-
-    //insert before playGame Button
-    this.playGameEl.insertAdjacentElement("beforebegin", gameDiv);
-    this.togglePlayGame("hide");
-  }
-
-  updateGuessesLeft() {
-    document.querySelector(".guesses-left").innerHTML = `
-      <h3>Guesses Left: ${gameState.currNumGuesses}</h3>
-    `;
-  }
-  changeGuessState(button, state) {
-    if (state === "initial") {
-      button.className = "input-letter";
-    } else if (state === "guessed") {
-      button.className = "input-letter guessed";
-    } else if (state === "correct") {
-      button.className = "input-letter correct";
-    }
-  }
-
-  togglePlayGame(status) {
-    if (status === "show") {
-      this.playGameEl.style = "display: visible";
-    } else {
-      this.playGameEl.style = "display: none";
-    }
-  }
-
-  gameOver(won, message) {
+function showWord() {
+  const wordLetters = word.split("");
+  wordLetters.forEach((letter, index) => {
     const div = document.createElement("div");
-    if (won) {
-      div.className = "result win";
-    } else {
-      div.className = "result loss";
-    }
-    div.appendChild(document.createTextNode(message));
+    div.className = "word-letter";
+    div.id = `letter-${index}`;
+    const result = correctLetters.includes(letter) ? letter : "";
+    div.appendChild(document.createTextNode(`${result}`));
 
-    document
-      .querySelector(".category")
-      .insertAdjacentElement("beforebegin", div);
-
-    setTimeout(() => {
-      div.remove();
-      document.querySelector(".game-container").remove();
-      this.togglePlayGame("show");
-    }, 7000);
-  }
-
-  reveal(letter, data) {
-    const letterDivs = document.querySelectorAll(`.word-letter`);
-    for (let i = 0; i < gameState.revealedLetters.length; i++) {
-      if (data.revealedLetters[i] !== null) {
-        if (gameState.revealedLetters[i] === null) {
-          letterDivs[i].innerHTML = `
-          <p>${letter}</p>
-          `;
-          letterDivs[i].className = "word-letter correct";
-        }
-      }
-    }
-  }
-
-  showInputLetters(gameDiv) {
-    const a = 65;
-    const z = 65 + 25;
-
-    const div = document.createElement("div");
-    div.className = "input-letters";
-    div.id = "input-letters";
-
-    for (let i = a; i <= z; i++) {
-      let button = document.createElement("button");
-      button.className = "input-letter";
-      button.id = String.fromCharCode(i);
-      button.dataset.status = "false";
-      button.textContent = button.id;
-
-      button.addEventListener("click", guess);
-      div.appendChild(button);
-    }
-
-    gameDiv.appendChild(div);
-  }
-}
-
-function guess(e) {
-  if (gameState.currNumGuesses != 0) {
-    // check guess
-    if (e.target.dataset.status === "false") {
-      let guess = {
-        gameId: gameState.id,
-        guess: e.target.id
-      };
-
-      fetch(`${serverUrl}/games/${gameState.id}`, {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify(guess)
-      })
-        .then(res => res.json())
-        .then(data => {
-          //correct guess
-          if (gameState.correctGuesses != data.correctGuesses) {
-            ui.changeGuessState(e.target, "correct");
-            ui.reveal(e.target.id, data);
-            updateGameState(data);
-            if (data.status === "WIN") {
-              ui.gameOver(
-                true,
-                `Game Over, You Win with ${gameState.currNumGuesses} guesses left!`
-              );
-            }
-          }
-          //wrong guess
-          else if (gameState.currNumGuesses != data.currNumGuesses) {
-            ui.changeGuessState(e.target, "guessed");
-            updateGameState(data);
-            ui.updateGuessesLeft();
-            if (data.status === "LOSS") {
-              ui.gameOver(
-                false,
-                `Game Over, You Lose! Word: ${gameState.word}`
-              );
-            }
-          }
-        })
-        .catch(err => console.log(err));
-    }
-  }
-  e.preventDefault();
-}
-
-let gameState;
-const ui = new UI();
-const serverUrl = "https://hangman-custom.herokuapp.com";
-
-// play new game event
-ui.playGameEl.addEventListener("click", startNewGame);
-function startNewGame(e) {
-  //hide new gae button
-  ui.playGameEl.style = "display: none";
-
-  //get word
-  fetch(`${serverUrl}/games`)
-    .then(res => res.json())
-    .then(data => {
-      gameState = data;
-      ui.initGame(gameState.word);
-    })
-    .catch(err => console.log(err));
-}
-
-function updateGameState(state) {
-  gameState = state;
+    wordContainer.appendChild(div);
+  });
 }
