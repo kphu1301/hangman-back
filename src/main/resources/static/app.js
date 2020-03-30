ecb6914197ee84e8ba207e39cb3044fa7d5b8017
klp1301 <ken.phu.217@my.csun.edu>
2020-03-30 00:43:12 -0700

    func

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,25 +1,69 @@
-//ui elements
-const playBtn = document.querySelector("#play-btn");
-const category = document.querySelector("#category");
-const bodyParts = document.querySelectorAll(".body-part");
-const lettersPlayed = document.querySelector("#letters-played");
-const wordContainer = document.querySelector(".word-container");
+function UI(word) {
+  //ui elements
+  this.word = word;
+  this.playBtn = document.querySelector(".play-btn");
+  this.category = document.querySelector("#category");
+  this.bodyParts = document.querySelectorAll(".body-part");
+  this.lettersPlayed = document.querySelector("#letters-played");
+  this.wordContainer = document.querySelector(".word-container");
+  this.container = document.querySelector(".container");
+  this.init();
+}
 
-const words = ["application", "pale", "background", "packers"];
-let correctLetters = ["a"];
-let word = words[Math.floor(Math.random() * words.length)];
+UI.prototype.init = function() {
+  this.togglePlayBtn();
+  this.showGameContainer();
+};
+UI.prototype.togglePlayBtn = function() {
+  this.playBtn.classList.toggle("hidden");
+};
 
-showWord();
+UI.prototype.showGameContainer = function() {
+  this.container.classList.toggle("hidden");
+  this.showWord();
+};
 
-function showWord() {
-  const wordLetters = word.split("");
+UI.prototype.showWord = function(correctLetters = []) {
+  const wordLetters = this.word.split("");
   wordLetters.forEach((letter, index) => {
     const div = document.createElement("div");
     div.className = "word-letter";
     div.id = `letter-${index}`;
-    const result = correctLetters.includes(letter) ? letter : "";
-    div.appendChild(document.createTextNode(`${result}`));
 
-    wordContainer.appendChild(div);
+    const result = correctLetters.includes(letter) ? letter : "";
+
+    const html = `
+      <p class="letter">${result}</p>
+    `;
+    div.innerHTML = html;
+
+    this.wordContainer.appendChild(div);
+  });
+};
+
+function Hangman(ui) {
+  this.words = ["application", "pale", "background", "packers"];
+  this.word = this.words[
+    Math.floor(Math.random() * this.words.length)
+  ].toUpperCase();
+  this.ui = new UI(this.word);
+  this.correctLetters = [];
+
+  document.querySelector(".input").addEventListener("click", e => {
+    if (e.target.classList.contains("input-letter")) {
+      //check if letter has been played
+      //guess logic
+    }
   });
 }
+
+function App() {
+  //add event listeners
+  //play new game
+  let game;
+  document.querySelector(".play-btn").addEventListener("click", () => {
+    game = new Hangman();
+  });
+}
+
+const app = new App();
